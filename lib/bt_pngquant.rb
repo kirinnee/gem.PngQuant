dbd7e71cfebcc900320a0ef1bf890405af3da171
kirinnee <kirinnee@gmail.com>
2019-03-09 14:49:29 +0800

    [Feat] Added PngQuant transformer

--- lib/bt_pngquant.rb
+++ lib/bt_pngquant.rb
@@ -1,6 +1,34 @@
 require "bt_pngquant/version"
+require "bt_pngquant/shell_helper"
+require "binary_transformer"
 
-module	BtPngquant
-  class Error < StandardError; end
-  # Your code goes here...
+using BT
+
+module BT
+	class PngQuant < BinaryTransformer::Transformer
+
+		def initialize(bit: 256, min: 65, max: 80, speed: 3, iebug: false)
+			raise ArgumentError.new("Bit cannot be smaller than 8") if bit < 8
+			raise ArgumentError.new("at least 0% quality") if min < 0 or max < 0
+			raise ArgumentError.new("at most 100% quality") if max > 100 or min > 100
+			raise ArgumentError.new("max must be larger than min") if min > max
+			raise ArgumentError.new("Lowest speed is 1") if speed < 1
+			raise ArgumentError.new("Maximum speed is 11") if speed > 11
+			@min = min
+			@max = max
+			@speed = speed
+			@ie_bug = iebug ? "--iebug " : ""
+			@bit = bit
+		end
+
+		def transform(bytes)
+			ret = "pngquant #{@bit} --speed #{@speed} #{@ie_bug}--quality=#{@min}-#{@max}" << bytes
+			ret.length > bytes.length ? bytes : ret
+		end
+
+		# @return [Array<String>]
+		def accepted_type
+			["image/png"]
+		end
+	end
 end
--- spec/bt_pngquant_spec.rb
+++ spec/bt_pngquant_spec.rb
@@ -1,5 +1,37 @@
 describe "BtPngquant" do
-	it "does something useful" do
-		expect(true).to eq(true)
+
+	it 'should raise exception if bit is smaller than 8' do
+		expect {BT::PngQuant.new(bit: 7)}.to raise_error ArgumentError, "Bit cannot be smaller than 8"
 	end
+
+	it 'should raise exception if either max or min is lower than 0' do
+		expect {BT::PngQuant.new(max: -1)}.to raise_error ArgumentError, "at least 0% quality"
+		expect {BT::PngQuant.new(min: -1)}.to raise_error ArgumentError, "at least 0% quality"
+	end
+
+	it 'should raise exception if either max or min is larger than 100' do
+		expect {BT::PngQuant.new(max: 101)}.to raise_error ArgumentError, "at most 100% quality"
+		expect {BT::PngQuant.new(min: 101)}.to raise_error ArgumentError, "at most 100% quality"
+	end
+
+	it 'should raise exception if min is larger than max' do
+		expect {BT::PngQuant.new(max: 10, min: 20)}.to raise_error ArgumentError, "max must be larger than min"
+	end
+
+	it 'should raise exception if speed is lower than 1' do
+		expect {BT::PngQuant.new(speed: 0)}.to raise_error ArgumentError, "Lowest speed is 1"
+	end
+
+	it 'should raise exception if speed is larger than 11' do
+		expect {BT::PngQuant.new(speed: 12)}.to raise_error ArgumentError, "Maximum speed is 11"
+	end
+
+	it 'should compress the image as expected' do
+		image = IO.binread "sophie.png"
+		image.extend StreamLike
+		expected = IO.binread "small.png"
+		quant = BT::PngQuant.new(speed: 11)
+		expect(image > quant).to eq(expected)
+	end
+
 end
